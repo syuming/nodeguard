7694e54d236ef3d4852ca2c92cab7fc0c9ca7510
Jason Chen <syuming@gmail.com>
2026-06-02 11:42:31 +0800

    fix: 服務啟動時自動開啟持續監控，不再需要手動開啟

--- monitor/apps.py
+++ monitor/apps.py
@@ -1,6 +1,18 @@
+import sys
 from django.apps import AppConfig
+
+_SKIP_CMDS = {"migrate", "makemigrations", "collectstatic", "shell", "createsuperuser"}
 
 
 class MonitorConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "monitor"
+
+    def ready(self):
+        if set(sys.argv) & _SKIP_CMDS:
+            return
+        from . import views
+        import threading
+        views._monitor_stop.clear()
+        views._monitor_thread = threading.Thread(target=views._monitor_loop, daemon=True)
+        views._monitor_thread.start()
